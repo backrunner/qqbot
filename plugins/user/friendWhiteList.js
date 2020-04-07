b4e58cc91219fac1245a0205af2df9f2f6bc25eb
BackRunner <12205593+backrunner@users.noreply.github.com>
2020-04-08 01:11:19 +0800

    fix: event name

--- plugins/user/friendWhiteList.js
+++ plugins/user/friendWhiteList.js
@@ -3,8 +3,8 @@ const { friendWhiteList } = require('../../bot.config');
 module.exports = {
     name: 'friend-white-list',
     apply: ctx => {
-        ctx.receiver.on('friend-add', (meta) => {
-            if (friendWhiteList.includes(meta.$userId)) {
+        ctx.receiver.on('request/friend', (meta) => {
+            if (friendWhiteList.includes(meta.userId)) {
                 meta.$approve();
             } else {
                 meta.$reject("您并不在我的好友白名单内，无法添加好友");
