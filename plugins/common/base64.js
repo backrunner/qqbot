7d122df765ce07f632c09b6c4932e764c06cf534
BackRunner <12205593+backrunner@users.noreply.github.com>
2020-04-08 10:41:51 +0800

    fix: fix trim number issue

--- plugins/common/base64.js
+++ plugins/common/base64.js
@@ -9,20 +9,20 @@ module.exports = {
             .option('-e, --encode <string>')
             .option('-d, --decode <code>')
             .action(({ meta, options }) => {
-                if (typeof options.encode != 'boolean') {
-                    let encode = options.encode.trim();
+                if (options.encode && typeof options.encode != 'boolean') {
+                    let encode = options.encode.toString().trim();
                     if (encode.length < 1) {
                         meta.$send(buildMessage(meta, '没有内容可以编码'));
                         return;
                     }
-                    meta.$send(buildMessage(meta, Buffer.from(options.encode, 'utf-8').toString('base64')));
-                } else if (typeof options.decode != 'boolean') {
-                    let code = options.decode.trim();
+                    meta.$send(buildMessage(meta, Buffer.from(encode, 'utf-8').toString('base64')));
+                } else if (options.decode && typeof options.decode != 'boolean') {
+                    let code = options.decode.toString().trim();
                     if (code.length < 1) {
                         meta.$send(buildMessage(meta, '没有内容可以解码'));
                         return;
                     }
-                    meta.$send(buildMessage(meta, Buffer.from(options.decode, 'base64').toString('utf-8')));
+                    meta.$send(buildMessage(meta, Buffer.from(code, 'base64').toString('utf-8')));
                 } else {
                     meta.$send(buildMessage(meta, '对于你刚才发送的指令，我理解不能'));
                 }
