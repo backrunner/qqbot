9ce9cb511b45e4253331d30b9923309a282019a6
BackRunner <12205593+backrunner@users.noreply.github.com>
2020-04-14 01:48:28 +0800

    fix: flow control

--- plugins/common/base64.js
+++ plugins/common/base64.js
@@ -10,19 +10,17 @@ module.exports = {
                 if (options.encode && typeof options.encode != 'boolean') {
                     let encode = options.encode.toString().trim();
                     if (encode.length < 1) {
-                        meta.$send(buildMessage(meta, '没有内容可以编码'));
-                        return;
+                        return meta.$send(buildMessage(meta, '没有内容可以编码'));
                     }
                     meta.$send(buildMessage(meta, Buffer.from(encode, 'utf-8').toString('base64')));
                 } else if (options.decode && typeof options.decode != 'boolean') {
                     let code = options.decode.toString().trim();
                     if (code.length < 1) {
-                        meta.$send(buildMessage(meta, '没有内容可以解码'));
-                        return;
+                        return meta.$send(buildMessage(meta, '没有内容可以解码'));
                     }
-                    meta.$send(buildMessage(meta, Buffer.from(code, 'base64').toString('utf-8')));
+                    return meta.$send(buildMessage(meta, Buffer.from(code, 'base64').toString('utf-8')));
                 } else {
-                    meta.$send(buildMessage(meta, '对于你刚才发送的指令，我理解不能'));
+                    return meta.$send(buildMessage(meta, '对于你刚才发送的指令，我理解不能'));
                 }
             });
     }
