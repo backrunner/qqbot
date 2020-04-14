794891903c2f41d52da870caec0fed5091087a96
BackRunner <12205593+backrunner@users.noreply.github.com>
2020-04-15 01:58:38 +0800

    add: random plugin

--- utils/random.js
+++ utils/random.js
@@ -1,6 +1,12 @@
+const csprng = require('csprng');
+
 module.exports = {
+    realRandomInt: (min, max) => {
+        let number = parseFloat(`0.${csprng(64, 10)}`);
+        return Math.floor(max - number * (max - min));
+    },
     randomInt: (min, max) => {
-        return Math.floor(Math.random() * (max - min)) + min;
+        return Math.floor(max - Math.random() * (max - min));
     },
     randomOperator: () => {
         return Math.random() < 0.5 ? '+' : '-';
