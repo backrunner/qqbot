2ab4df29bf7c17771141df5eb5e7660f1f99512a
BackRunner <12205593+backrunner@users.noreply.github.com>
2020-05-13 15:26:33 +0800

    update: config template

--- bot.config.tmpl.js
+++ bot.config.tmpl.js
@@ -1,11 +1,25 @@
 module.exports = {
     basic_command_reply: true,
     welcome_message: '欢迎入群~',
+    auto_confirm_apply: {
+        enable: false,
+        all_enable: true,
+        enable_groups: []
+    },
+    auto_reject_apply: {
+        enable: true,
+        all_enable: true,
+        enable_groups: [],
+        reject_message: '',
+    },
     sensitive_words: {
         deleteMsg: false,
         banUser: true,
         banTime: 300,
         word_list: []
     },
-    friendWhiteList: []
+    friendWhiteList: [],
+    qrcode: {
+        timeout: 30
+    }
 };
