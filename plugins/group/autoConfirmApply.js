f0c801f8e2ed049695296c6f1a1ae4de6f0ee972
BackRunner <12205593+backrunner@users.noreply.github.com>
2020-05-13 15:33:19 +0800

    update: move config to bot.config

--- plugins/group/autoConfirmApply.js
+++ plugins/group/autoConfirmApply.js
@@ -1,18 +1,21 @@
-const all_enable = true;
-const enable_groups = [];
+const config = require('../../bot.config').auto_confirm_apply;
+const enable = config.auto_confirm_apply.enable;
+const all_enable = config.all_enable;
+const enable_groups = config.enable_groups;
 
 module.exports = {
     name: 'auto-confirm-apply',
     apply: ctx => {
         ctx.receiver.on('request/group/add', (meta) => {
-            if (all_enable) {
-                // 自动同意申请
-                meta.$approve();
-            } else {
-                if (enable_groups.includes(meta.groupId)) {
-                    meta.$approve();
+            if (!enable) {
+                return;
+            }
+            if (!all_enable) {
+                if (!enable_groups.includes(meta.groupId)) {
+                    return;
                 }
             }
+            meta.$approve();
         });
     }
 };
